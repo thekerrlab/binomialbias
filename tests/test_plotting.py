19bd80e7194bc948b41a7a3cb4e678bf3c640f80
Cliff Kerr <questionably@gmail.com>
2023-09-15 10:39:04 -0400

    add plot_bias test

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,13 +1,13 @@
-'''
+"""
 Simple tests of plotting
-'''
+"""
 
 import sciris as sc
 import binomialbias as bb
 
 
 def test_plotting(show=False):
-    ''' Test several different plotting options '''
+    """ Test several different plotting options """
     
     sc.options(interactive=show)
     
@@ -25,7 +25,15 @@ def test_plotting(show=False):
     return out
 
 
+def test_plot_bias(show=False):
+    """ Test function example """
+    sc.options(interactive=show)
+    B = bb.plot_bias(n=20, n_e=10, n_a=7, show=show)
+    return B
+
+
 if __name__ == '__main__':
     out = test_plotting(show=True)
+    B = test_plot_bias(show=True)
 
     
